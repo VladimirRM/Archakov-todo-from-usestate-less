64b4b22644bb643cc1d9ea20905a259476413f4a
Vladimir RM <shvdrummer@gmail.com>
2023-01-19 12:03:03 +0200

    add buttons delete & edit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import "./App.css";
 import "./index.css";
 
 function App() {
-  const [task, setTask] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   const addTask = () => {
-    const value = value;
+    const value = text;
   };
   return (
     <div className="App">
       <input
         type="text"
         placeholder="Enter something..."
-        onChange={(e) => setTask(e.target.value)}
-        value={task}
+        onChange={(e) => setTasks(e.target.value)}
+        value={tasks}
       />
       <button onClick={addTask}>Add task</button>
+        {tasks.map(task,id=>(
+          <ul>
+            <li>{task}
+      <button onClick={()=>handleDelete(id)}>Delete</button>
+      <button onClick={()=>handleEdit(id)}>Edit</button>
+
+            
+            
+            </li>
+          </ul>
+        ))}
+
     </div>
   );
 }
