67c9c90276d2419bdede2420595553d7ea957d39
Vladimir RM <shvdrummer@gmail.com>
2023-01-19 11:18:28 +0200

    create input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React,{useState} from 'react';
-import './App.css';
-import './index.css'
+import React, { useState } from "react";
+import "./App.css";
+import "./index.css";
 
 function App() {
-  const [task,setTask] = useState([])
+  const [task, setTask] = useState([]);
 
-  const addTask = ()=>{
-    
-  }
+  const addTask = () => {
+    const value = value;
+  };
   return (
     <div className="App">
-   
+      <input
+        type="text"
+        placeholder="Enter value"
+        onChange={(e) => setTask(e.target.value)}
+        value={value}
+      />
     </div>
   );
 }
