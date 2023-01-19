fc2c9dfbb303f0bc479253f6598db6dfabe3fb29
Vladimir RM <shvdrummer@gmail.com>
2023-01-19 16:36:42 +0200

    change idea function

--- src/App.js
+++ src/App.js
@@ -6,29 +6,33 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const addTask = () => {
-    const value = text;
-  };
+    const text = prompt("text task");
+    setTasks([...tasks, text])
+  }; 
+  const handleDelete =(task,id)=>{
+       setTasks(tasks.filter(task=>task.id !==id))
+  }
+  const handleEdit =()=>{
+
+  }
   return (
     <div className="App">
-      <input
+      {/* <input
         type="text"
         placeholder="Enter something..."
         onChange={(e) => setTasks(e.target.value)}
-        value={tasks}
-      />
+        value={text}
+      /> */}
       <button onClick={addTask}>Add task</button>
-        {tasks.map(task,id=>(
-          <ul>
-            <li>{task}
-      <button onClick={()=>handleDelete(id)}>Delete</button>
-      <button onClick={()=>handleEdit(id)}>Edit</button>
-
-            
-            
-            </li>
-          </ul>
-        ))}
-
+      {tasks.map(task => (
+        <ul key={task.id}>
+          <li key={task.id}>
+            {task}
+            <button onClick={() => handleDelete(task.id)}>Delete</button>
+            <button onClick={() => handleEdit(task.id)}>Edit</button>
+          </li>
+        </ul>
+      ))}
     </div>
   );
 }
